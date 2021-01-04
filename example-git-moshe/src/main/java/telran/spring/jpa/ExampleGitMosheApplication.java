04e5ed52627361861e0ad35e7faa1e95af2e3cb6
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-04 17:57:34 +0200

    gggg

--- example-git-moshe/src/main/java/telran/spring/jpa/ExampleGitMosheApplication.java
+++ example-git-moshe/src/main/java/telran/spring/jpa/ExampleGitMosheApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExampleGitMosheApplication {
-
+//hjhjhj
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleGitMosheApplication.class, args);
 	}
