cea6214b7e63b2388895818c50e315af4d4795c8
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-04 15:55:49 +0200

    initial code

---
+++ example-git-moshe/src/test/java/telran/spring/jpa/ExampleGitMosheApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.spring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitMosheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
